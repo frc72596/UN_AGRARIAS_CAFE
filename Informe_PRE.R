8f58734078fdda86e820e65054d17c85dc446207
Your Name <feramirezco@unal.edu.co>
2025-09-09 20:16:42 -0500

    Arreglos

--- Informe_PRE.R
+++ Informe_PRE.R
@@ -37,6 +37,22 @@ dfSummary(ICC_MID)
 dfSummary(ICC_END)
 # Analisis en paralelo
 N_FACTORS(I_PRE)
-EFA(I_PRE,3,method= "ULS",rotation = "oblimin")
-reliability(I_PRE,nfactors = 3)
-omega(I_PRE,nfactors = 3)
+M_T<- "
+Determinantes =~ ICC1 + ICC8 + ICC9 + ICC13 + ICC15
+Valoracion   =~ ICC2 + ICC3 + ICC11 + ICC12 + ICC14
+Practicas    =~ ICC4 + ICC5 + ICC6 + ICC7 + ICC10
+Determinantes ~~ Valoracion
+Determinantes ~~ Practicas
+Valoracion    ~~ Practicas
+"
+MT<-cfa(M_T, data = I_PRE, estimator = "MLR") 
+summary(MT, fit.measures = TRUE, standardized = TRUE)
+fitMeasures(MT)
+lavaanPlot(MT,stand = TRUE,stars = "regression",covs = TRUE,coefs = TRUE)
+ME<-fa(I_PRE, nfactor=3, cor="poly", fm="minres", rotate = "oblimin")
+fa.diagram(ME)
+# Se eliminan los items 1,6,13, 14 y 15
+I_PRE_2 <- I_PRE[, !(names(I_PRE) %in% c("ICC1","ICC6", "ICC13", "ICC14", "ICC15"))]
+N_FACTORS(I_PRE_2)
+EFA(I_PRE_2,2,method= "ULS",rotation = "oblimin")
+irt.fa(I_PRE_2,nfactors = 2, fm="minres",rotate = "oblimin")
